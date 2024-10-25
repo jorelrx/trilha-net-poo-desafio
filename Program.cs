3b2b8b98105fb692423fea7a59ac1810371508d7
Joel Victor <jnull.rx@gmail.com>
2024-10-25 02:08:12 -0300

    Implementando features

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new ("12 934567891", "Nokia 3310", "Preto", 123456789);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("Smartphone Iphone: ");
+Iphone iphone = new ("12 987654321", "Iphone 15", "Branco", 987654321);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
